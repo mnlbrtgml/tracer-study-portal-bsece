ae438ae7c414969188f0be62c9d7f979aa907a8c
Mon Albert Gamil <mrgamilmonalbert@gmail.com>
2023-11-18 03:55:11 +0800

    update error view

--- src/views/error/IndexView.vue
+++ src/views/error/IndexView.vue
@@ -1,3 +1,13 @@
 <template>
-  <h1 class="text-3xl font-bold underline">Hello error page!</h1>
+  <section>
+    <div
+      class="container h-[calc(100dvh-1rem)] mx-auto p-2 flex flex-col items-center justify-center text-center md:h-[calc(100dvh-2rem)] md:p-4 lg:h-[calc(100dvh-4rem)] lg:p-8"
+    >
+      <img src="@/assets/images/ErrorIcon.svg" alt="" class="w-full aspect-video max-w-xl" />
+
+      <p class="text-blue-700 p-4 text-xl font-bold">
+        Looks like you've found the doorway to the great nothing
+      </p>
+    </div>
+  </section>
 </template>
